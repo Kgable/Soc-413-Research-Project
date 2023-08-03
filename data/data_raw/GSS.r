5c85d3a6b681b16ef5bd0602fe97282f837c2f83
Kevin Gable <kgable@uoregon.edu>
2023-08-03 13:54:09 -0700

    Additional raw data and organization.

--- data/data_raw/GSS.r
+++ data/data_raw/GSS.r
@@ -25,7 +25,7 @@ library(foreign)
   }
 
   read.dat <- function(dat, metadata_var, labels.included = "yes") {
-      read.fwf(dat, widths = metadata_var[["ColWidth"]], col.names = metadata_var[["ColName"]])
+      read.table(dat, col.names = metadata_var[["ColName"]])
   }
 
 
